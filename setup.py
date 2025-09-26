a82918f44ada9b79a0e0cfcb531d80bf8467ca54
santhosh kumar <santhosh173273@gmail.com>
2025-09-26 11:49:27 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = 'Generative AI Project',
+  version = '0.0.0',
+  author = 'santhosh kumar',
+  author_email = 'santhosh173273@gmail.com',
+  packages = find_packages(),
+  install_requires = []
+)
